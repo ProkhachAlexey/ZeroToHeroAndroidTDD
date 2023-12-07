c5e2fb0795cb9be97b0231ea609bfe9373d899c6
ProkhachAlexey <asprokhach68@gmail.com>
2023-12-07 22:08:25 +0300

    task/008-hide-text solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,32 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+        binding.hideButton.setOnClickListener {
+            binding.titleTextView.visibility = View.GONE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY, binding.titleTextView.visibility)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val textVisibility = savedInstanceState.getInt(KEY, View.VISIBLE)
+        binding.titleTextView.visibility = textVisibility
+    }
+
+    private companion object {
+        const val KEY = "key"
     }
 }
