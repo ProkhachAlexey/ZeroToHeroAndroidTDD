d49755c83ba51f836ff0fd1dd420162da0e21b31
ProkhachAlexey <asprokhach68@gmail.com>
2023-12-08 18:27:27 +0300

    task/009-remove-textview solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,39 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var isTextViewVisible = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.removeButton.setOnClickListener {
+            val parentLayout = binding.titleTextView.parent as ViewGroup
+            parentLayout.removeView(binding.titleTextView)
+            isTextViewVisible = false
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(KEY, isTextViewVisible)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        isTextViewVisible = savedInstanceState.getBoolean(KEY)
+        if (!isTextViewVisible) {
+            val parentLayout = binding.titleTextView.parent as ViewGroup
+            parentLayout.removeView(binding.titleTextView)
+        }
+    }
+
+    private companion object {
+        const val KEY = "key"
     }
 }
